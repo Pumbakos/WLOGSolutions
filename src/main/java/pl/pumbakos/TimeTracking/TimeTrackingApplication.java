78d7a19325fd57c310ae1cd408e55a0239697ca7
Pumbakos <dawid.palubiak@gmail.com>
2021-08-02 14:26:20 +0200

    Fix static return at /hello

--- src/main/java/pl/pumbakos/TimeTracking/TimeTrackingApplication.java
+++ src/main/java/pl/pumbakos/TimeTracking/TimeTrackingApplication.java
@@ -2,14 +2,12 @@ package pl.pumbakos.TimeTracking;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class TimeTrackingApplication {
 
--- src/main/java/pl/pumbakos/TimeTracking/springboot/HelloController.java
+++ src/main/java/pl/pumbakos/TimeTracking/springboot/HelloController.java
@@ -1,13 +1,30 @@
 package pl.pumbakos.TimeTracking.springboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
     public String index(){
         return "<h1>Hello World</h1>";
     }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name", required = false, defaultValue = "Hello there!")
+                        String name, Model model){
+        model.addAttribute(name);
+
+        return "Hello, "+ name;
+    }
+
+
 }
+
+
+
